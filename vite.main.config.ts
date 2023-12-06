668d9584901caeb60df66180f9c2928ad3d5f7da
Sai Koukuntla <sai.koukunt@gmail.com>
2023-12-06 01:10:38 -0500

    debugging config

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
+  base: `/src/`,
   resolve: {
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     browserField: false,
     mainFields: ['module', 'jsnext:main', 'jsnext'],
+  },
+  build: {
+    sourcemap: true,
   },
 });
